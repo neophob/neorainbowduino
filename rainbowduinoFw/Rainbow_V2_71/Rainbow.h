68b1ee7920154adec4eba145c77466f3e0a3ef6e
michu@neophob.com <michu@neophob.com@0048bdbb-7a4a-2424-8bf1-d9e10754f234>
2011-01-09 22:25:34 +0000

    cleanup defines, add comment

--- rainbowduinoFw/Rainbow_V2_71/Rainbow.h
+++ rainbowduinoFw/Rainbow_V2_71/Rainbow.h
@@ -7,13 +7,16 @@
 //=============================================
 //PORTC maps to Arduino analog pins 0 to 5. Pins 6 & 7 are only accessible on the Arduino Mini
 //PORTC - The Port C Data Register - read/write
-#define SH_PORT   PORTC
 
 #define SH_BIT_SDI   0x01
+#define SH_BIT_SDI_I 0xFE
 #define SH_BIT_CLK   0x02
+#define SH_BIT_CLK_I 0xFD
 
 #define SH_BIT_LE    0x04
+#define SH_BIT_LE_I  0xFB
 #define SH_BIT_OE    0x08
+#define SH_BIT_OE_I  0xF7
 
 //============================================
 
@@ -24,16 +27,23 @@
 //Checking if a bit is set: if (byte & (1 << bit))
 //Checking if a bit is cleared: if (~byte & (1 << bit)) OR if (!(byte & (1 << bit)))
 
-//potential take too long! -> PORTC&=~0x02;PORTC|=0x02
-#define CLK_RISING  {SH_PORT &=~ SH_BIT_CLK; SH_PORT |= SH_BIT_CLK;}
-#define LE_HIGH     {SH_PORT |= SH_BIT_LE;}
-#define LE_LOW      {SH_PORT &=~ SH_BIT_LE;}
-#define ENABLE_OE   {SH_PORT &=~ SH_BIT_OE;}
-#define DISABLE_OE  {SH_PORT |= SH_BIT_OE;}
+//potential take too long! -> PORTC &=~0x02; PORTC|=0x02
+//Clock input terminal for data shift on rising edge
+#define CLK_RISING  {PORTC &= SH_BIT_CLK_I; PORTC |= SH_BIT_CLK;}
 
-#define SHIFT_DATA_1     {SH_PORT |= SH_BIT_SDI;}
+//Data strobe input terminal, Serial data is transfered to the respective latch when LE is high. 
+//The data is latched when LE goes low.
+#define LE_HIGH     {PORTC |= SH_BIT_LE;}
+#define LE_LOW      {PORTC &= SH_BIT_LE_I;}
+
+//Output Enabled, when (active) low, the output drivers are enabled; 
+//when high, all output drivers are turned OFF (blanked).
+#define ENABLE_OE   {PORTC &= SH_BIT_OE_I;}
+#define DISABLE_OE  {PORTC |= SH_BIT_OE;}
+
+#define SHIFT_DATA_1     {PORTC |= SH_BIT_SDI;}
 //potential take too long! -> PORTC&=~0x01
-#define SHIFT_DATA_0     {SH_PORT &=~ SH_BIT_SDI;}
+#define SHIFT_DATA_0     {PORTC &= SH_BIT_SDI_I;}
 
 #endif
 
